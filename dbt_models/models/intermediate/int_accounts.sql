cef89e168fec3886476f741509bba94c21ada58c
sarang <sarang.suknale1995@gmail.com>
2025-12-26 17:40:13 +0530

    int_account_added

---
+++ dbt_models/models/intermediate/int_accounts.sql
@@ -0,0 +1,37 @@
+with src1 as (
+    select 
+          account_id,
+          user_id,
+          concat('XXXX', right(account_number, 4)) as account_number_last4,
+          account_type,
+          account_subtype,
+          account_status,
+          balance,
+          available_balance,
+          (balance-available_balance) as balance_diffrence,
+          ifnull(available_credit, 0) as available_credit,
+          cast(case 
+                   when (balance + available_credit) > 0 
+                   then (balance / (balance + available_credit)) 
+                   else 0 
+               end as number(10,2)) as credit_utilization_pct,
+          currency,
+          balance_date,
+          datediff('day', balance_date, current_date()) as days_since_balance_check,
+          aggregation_success_date,
+          datediff('day', aggregation_success_date, current_date()) as days_since_aggregation_success,
+          case 
+              when aggregation_success_date < dateadd(day, -30, current_date())
+              then True
+              else False
+          end as is_data_stale,
+          aggregation_attempt_date,
+          datediff('day', aggregation_attempt_date, current_date()) as days_since_aggregation_attempt,
+          created_date,
+          datediff('day', created_date, current_date()) as account_age_in_days,
+          datediff('month', created_date, current_date()) as account_age_in_months,
+          last_updated
+    from {{ref('stg_finicity_accounts')}}
+)
+
+select * from src1
