ddb32f8da2a6b34951655e3c1a05f56904199347
sarang <sarang.suknale1995@gmail.com>
2025-12-31 13:44:16 +0530

    int_src3_transactions added

---
+++ dbt_models/models/intermediate/int_src3_transactions.sql
@@ -0,0 +1,18 @@
+with src3_txt as (
+    select
+          transaction_id,
+          account_id,
+          transaction_date,
+          datediff('month', transaction_date, current_date()) as transaction_age_in_months,
+          amount,
+          category,
+          merchant,
+          is_manual,
+          is_physical as is_verified,
+          post_date,
+          last_updated,
+          datediff('day', last_updated, current_date()) as days_since_updated
+    from {{ref('stg_yodlee_transactions')}}
+)
+
+select * from src3_txt
