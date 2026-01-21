0c7cbba28bee6f15cae1c8a25fafb19fdc6b30ed
sarang <sarang.suknale1995@gmail.com>
2026-01-21 20:54:27 +0530

    Modified incremental logic in test env

--- dbt_models/models/marts/alerts/fct_alerts.sql
+++ dbt_models/models/marts/alerts/fct_alerts.sql
@@ -1,11 +1,21 @@
 
-{{
+{{ 
     config(
-        materialized = 'incremental' if target.name == 'prod' else 'view',
+        materialized = 'incremental' if target.name in ['prod', 'test'] else 'view',
         incremental_strategy = 'merge',
         unique_key = 'id',
-        on_schema_change='sync_all_columns'
-    )
+        on_schema_change = 'sync_all_columns',
+        post_hook = (
+                [
+                    "delete from {{ this }} "
+                    "where alert_sent_date < ("
+                    "  select dateadd(month, -2, coalesce(max(alert_sent_date), to_date('2020-01-01')))"
+                    "  from {{ this }}"
+                    ")"
+                ]
+                if target.name == 'test' else []
+            )
+    ) 
 }}
 
 with fct_alerts as (
@@ -23,10 +33,16 @@ with fct_alerts as (
           is_pending
     from {{ref('int_alerts')}} 
 
-    {% if is_incremental() %}
-    where alert_sent_date >= 
-          (select dateadd('day', -15, max(alert_sent_date)) 
-          from {{this}})
+     {% if is_incremental() and target.name == 'prod' %}
+        where alert_sent_date >= (
+            select dateadd('day',-15, max(alert_sent_date))
+            from {{ this }}
+        )
+      {% elif is_incremental() and target.name == 'test' %}
+        where alert_sent_date >= (
+            select dateadd('day', -90, max(alert_sent_date))
+            from {{ this }}
+        )
     {% endif %}
 )
 
